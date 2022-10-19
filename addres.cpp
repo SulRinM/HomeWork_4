24df3f6956dad4cd151f232a8bad603f748527c2
SulRinM <108032806+SulRinM@users.noreply.github.com>
2022-10-19 20:54:42 +0300

    Create addres.cpp

---
+++ addres.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class addres
+{
+public:
+    std::string city;
+    std::string street;
+    int house_num;
+    int apart_num;
+    std::string get_output_address();
+private:
+};
+
+std::string addres::get_output_address()
+{
+    std::string data_addres = city + ", " + street + ", " +
+                              std::to_string(house_num) + ", " + std::to_string(apart_num);
+    return data_addres;
+}
+
+int main()
+{
+    std::ifstream fin("in.txt");
+    if (fin.is_open())
+    {
+        int size;
+        fin >> size;
+        addres *arr = new addres[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            fin >> arr[i].city;
+            fin >> arr[i].street;
+            fin >> arr[i].house_num;
+            fin >> arr[i].apart_num;
+        }
+        fin.close();
+
+        std::ofstream fout ("out.txt"); 
+        fout << size << std::endl;
+        for (int k = size - 1; k >= 0; k--)
+        {
+            fout << arr[k].get_output_address() << std::endl;
+        }
+        fout.close();
+        delete [] arr;
+    }
+    else {
+        std::cout << "Не получилось открыть файл!" << std::endl;
+    }
+
+    return 0;
+}
